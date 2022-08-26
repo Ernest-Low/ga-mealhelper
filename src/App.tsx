caaee645abfd064e0d2dbaa9cd02d1bb72d45939
Ernest-Low <ernestlow32@gmail.com>
2022-08-26 15:39:31 +0800

    Testing vercel compatiability with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,29 @@
-import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  interface user {
+    name: String;
+    id: Number;
+  }
+
+  const user2 = {
+    name: "Bruce",
+    id: 1,
+  };
+  useEffect(() => {
+    const apicall = async () => {
+      const res = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/random.php"
+      );
+      const json = await res.json();
+      console.dir(json);
+    };
+
+    apicall();
+  }, []);
+
+  console.dir(user2);
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
