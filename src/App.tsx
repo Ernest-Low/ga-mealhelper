406662258a8eeb3fefe85192c5847a59d26c9d79
Ernest-Low <ernestlow32@gmail.com>
2022-08-29 01:56:39 +0800

    Transfer to github to send to laptop

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import Homepage from "./pages/Homepage";
+import saveRecipe from "./modules/saveRecipe";
+import { Jsontype, Recipetype } from "./interfaces";
 
 function App() {
-  interface user {
-    name: String;
-    id: Number;
-  }
+  const [recipe, setrecipe] = useState<Recipetype>();
 
   const user2 = {
     name: "Bruce",
     id: 1,
   };
+
   useEffect(() => {
     const apicall = async () => {
       const res = await fetch(
         "https://www.themealdb.com/api/json/v1/1/random.php"
       );
-      const json = await res.json();
+      const json : Jsontype = await res.json() as Jsontype;
       console.dir(json);
+      setrecipe(saveRecipe(json));
+      console.dir(recipe);
     };
 
     apicall();
@@ -27,6 +30,7 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Homepage />
     </div>
   );
 }
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <>
+      <h1>This is homepage content rendering</h1>
+    </>
+  );
+};
+
+export default Homepage;
